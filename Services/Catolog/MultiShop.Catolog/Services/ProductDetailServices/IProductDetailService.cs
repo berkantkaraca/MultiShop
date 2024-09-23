6e5b46a0240dd67e67e28a7c52c38b81b8bc0549
Berkant Karaca <bkaraca.71m@gmail.com>
2024-09-23 16:48:49 +0300

    Added ProductDetail service

---
+++ Services/Catolog/MultiShop.Catolog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catolog.Dtos.ProductDetailDtos;
+
+namespace MultiShop.Catolog.Services.ProductDetailServices
+{
+    public interface IProductDetailService
+    {
+        Task<List<ResultProductDetailDto>> GetAllProductDetailAsync();
+        Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto);
+        Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto);
+        Task DeleteProductDetailAsync(string id);
+        Task<GetByIdProductDetailDto> GetProductDetailByIdAsync(string id);
+    }
+}
