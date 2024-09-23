6c09bfc28794183dbf3096565ba65a36b5769989
Berkant Karaca <bkaraca.71m@gmail.com>
2024-09-23 16:15:00 +0300

    Added category service

---
+++ Services/Catolog/MultiShop.Catolog/Services/CategoryServices/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.Catolog.Dtos.CategoryDtos;
+
+namespace MultiShop.Catolog.Services.CategoryServices
+{
+    public interface ICategoryService
+    {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(string id);
+        Task<GetByIdCategoryDto> GetCategoryByIdAsync(string id);
+    }
+}
