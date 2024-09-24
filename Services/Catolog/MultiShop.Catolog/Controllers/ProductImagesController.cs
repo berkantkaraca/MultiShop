739ff997551ec20120cbe02f1554b1f95d4585ae
Berkant Karaca <bkaraca.71m@gmail.com>
2024-09-24 18:37:04 +0300

    Added product image controller

---
+++ Services/Catolog/MultiShop.Catolog/Controllers/ProductImagesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catolog.Dtos.ProductImageDtos;
+using MultiShop.Catolog.Services.ProductImageServices;
+
+namespace MultiShop.Catolog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly IProductImageService _ProductImageService;
+
+        public ProductImagesController(IProductImageService ProductImageService)
+        {
+            _ProductImageService = ProductImageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductImageList()
+        {
+            var values = await _ProductImageService.GetAllProductImageAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductImageById(string id)
+        {
+            var values = await _ProductImageService.GetProductImageByIdAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProductImage(CreateProductImageDto createProductImageDto)
+        {
+            await _ProductImageService.CreateProductImageAsync(createProductImageDto);
+            return Ok("Ürün görselleri başarıyla eklendi.");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductImage(string id)
+        {
+            await _ProductImageService.DeleteProductImageAsync(id);
+            return Ok("Ürün görselleri başarıyla silindi.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductImage(UpdateProductImageDto updateProductImageDto)
+        {
+            await _ProductImageService.UpdateProductImageAsync(updateProductImageDto);
+            return Ok("Ürün görselleri başarıyla güncellendi.");
+        }
+
+    }
+}
