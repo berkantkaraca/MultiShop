bb4821bfd28c287b1c58171b6f03f6c3f7161272
Berkant Karaca <bkaraca.71m@gmail.com>
2024-09-26 12:00:17 +0300

    Added configurations

--- Services/Catolog/MultiShop.Catolog/Program.cs
+++ Services/Catolog/MultiShop.Catolog/Program.cs
@@ -1,6 +1,25 @@
+using Microsoft.Extensions.Options;
+using MultiShop.Catolog.Services.CategoryServices;
+using MultiShop.Catolog.Services.ProductDetailServices;
+using MultiShop.Catolog.Services.ProductImageServices;
+using MultiShop.Catolog.Services.ProductServices;
+using MultiShop.Catolog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
