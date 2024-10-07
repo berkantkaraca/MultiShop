5f8c023cff0779b277e6e1ac0f78aac6c3e2ba37
Berkant Karaca <bkaraca.71m@gmail.com>
2024-10-07 21:19:42 +0300

    Add order detail cqrs classes

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/GetOrderDetailByIdQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.OrderDetailQueries;
+using MultiShop.Order.Application.Features.CQRS.Result.OrderDetailResuts;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers
+{
+    public class GetOrderDetailByIdQueryHandler
+    {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public GetOrderDetailByIdQueryHandler(IRepository<OrderDetail> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetOrderDetailByIdQueryResult> Handle(GetOrderDetailByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+
+            return new GetOrderDetailByIdQueryResult
+            {
+                OrderDetailId = values.OrderDetailId,
+                ProductAmount = values.ProductAmount,
+                OrderingId = values.OrderingId,
+                ProductId = values.ProductId,
+                ProductName = values.ProductName,
+                ProductPrice = values.ProductPrice,
+                ProductTotalPrice = values.ProductTotalPrice,
+            };
+        }
+    }
+}
