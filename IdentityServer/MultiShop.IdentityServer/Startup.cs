b57359fe409b68af6f73d0d66dbc8449338c05ef
Berkant Karaca <bkaraca.71m@gmail.com>
2024-10-18 15:11:58 +0300

    Add api resources registiration

--- IdentityServer/MultiShop.IdentityServer/Startup.cs
+++ IdentityServer/MultiShop.IdentityServer/Startup.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+﻿ // Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
@@ -50,6 +50,7 @@ namespace MultiShop.IdentityServer
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiResources(Config.ApiResources)
                 .AddAspNetIdentity<ApplicationUser>();
 
             // not recommended for production - you need to store your key material somewhere secure
