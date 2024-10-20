82f3a006c64062d416f7d5911ef97fb539e8673c
Berkant Karaca <bkaraca.71m@gmail.com>
2024-10-20 15:52:55 +0300

    Add manager classes

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Concrete/CargoCustomerManager.cs
@@ -0,0 +1,46 @@
+﻿using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoCustomerManager : ICargoCustomerService
+    {
+        private readonly ICargoCustomerDal _cargoCustomer;
+
+        public CargoCustomerManager(ICargoCustomerDal cargoCustomerDal)
+        {
+            _cargoCustomer = cargoCustomerDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _cargoCustomer.Delete(id);
+        }
+
+        public List<CargoCustomer> TGetAll()
+        {
+            return _cargoCustomer.GetAll();
+        }
+
+        public CargoCustomer TGetById(int id)
+        {
+            return _cargoCustomer.GetById(id);
+        }
+
+        public void TInsert(CargoCustomer entity)
+        {
+            _cargoCustomer.Insert(entity);
+        }
+
+        public void TUpdate(CargoCustomer entity)
+        {
+            _cargoCustomer.Update(entity);
+        }
+    }
+}
---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Concrete/CargoOperationManager.cs
@@ -0,0 +1,46 @@
+﻿using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DataAccessLayer.Abstract;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Concrete
+{
+    public class CargoOperationManager : ICargoOperationService
+    {
+        private readonly ICargoOperationDal _cargoOperation;
+
+        public CargoOperationManager(ICargoOperationDal cargoOperationDal)
+        {
+            _cargoOperation = cargoOperationDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _cargoOperation.Delete(id);
+        }
+
+        public List<CargoOperation> TGetAll()
+        {
+            return _cargoOperation.GetAll();
+        }
+
+        public CargoOperation TGetById(int id)
+        {
+            return _cargoOperation.GetById(id);
+        }
+
+        public void TInsert(CargoOperation entity)
+        {
+            _cargoOperation.Insert(entity);
+        }
+
+        public void TUpdate(CargoOperation entity)
+        {
+            _cargoOperation.Update(entity);
+        }
+    }
+}
